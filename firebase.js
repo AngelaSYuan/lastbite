f1a73857108eddd52705f006a8a11acdc7040c9e
Angela Yuan <Angelayuan1210@gmail.com>
2024-03-11 11:45:48 -0400

    firebase setup, restaurants can log in

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyByGyL0XDhu9RCok8n48XRm-up5ttsgAtI",
+  authDomain: "lastbite-fca2a.firebaseapp.com",
+  projectId: "lastbite-fca2a",
+  storageBucket: "lastbite-fca2a.appspot.com",
+  messagingSenderId: "601062709776",
+  appId: "1:601062709776:web:7dd1bb9444230d9ce18895"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
