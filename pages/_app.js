f1d2c4ce380bd2270159a0b4e0d960a31d087b16
Angela Yuan <Angelayuan1210@gmail.com>
2024-03-18 00:55:48 -0400

    geist font

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,18 @@
 // _app.js
 
 import { loadStripe } from '@stripe/stripe-js';
+import '../styles/geist.css'; // Import the Geist UI CSS file
 
 const stripePromise = loadStripe("pk_test_51OtLFw2NW6lHOE0BZZAo8tivFJpcwlf4BcJ5jVsBfGaytvOfNgDiYev6usuCtfrBTsy6iUXDDXjfujtYwJUGtGCW00CbBqzUMA");
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export { stripePromise };
 export default MyApp;
+
